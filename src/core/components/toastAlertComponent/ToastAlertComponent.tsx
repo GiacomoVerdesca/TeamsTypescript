992bf8cdc96480dac3d069adfcf2ef6e4418cfe7
Gian20luca <gianluca.bellafronte@gmail.com>
2021-03-03 16:16:17 +0100

    fixed ToastAlertComponent

--- src/core/components/toastAlertComponent/ToastAlertComponent.tsx
+++ src/core/components/toastAlertComponent/ToastAlertComponent.tsx
@@ -16,22 +16,22 @@ export const ToastAlertComponent = (props: any) => {
   const sendEmailResponse = useSelector(sendEmailSelector);
   const createEventResponse = useSelector(createEventSelector);
 
-  const toastSuccess = (success: string, actionDispatch: any) => toast.success(success, {
+  const toastSuccess = (success: string) => toast.success(success, {
     className: 'toastSuccess',
     position: "top-center",
     autoClose: 5000,
     hideProgressBar: false,
-    closeOnClick: actionDispatch,
+    closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
   })
 
-  const toastError = (error: string, actionDispatch: any) => toast.error(error, {
+  const toastError = (error: string) => toast.error(error, {
     position: "top-center",
     autoClose: 5000,
     hideProgressBar: false,
-    closeOnClick: actionDispatch,
+    closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
@@ -40,13 +40,13 @@ export const ToastAlertComponent = (props: any) => {
   return (
     <div>
 
-      {sendEmailResponse.success ? toastSuccess('Email inviata correttamente!', dispatch(setSuccessEmail(false)))
-        : onlineMeetingResponse.success ? toastSuccess('Meeting creato correttamente!', dispatch(setSuccessMeeting(false)))
-          : createEventResponse.success ? toastSuccess('Evento creato correttamente!', dispatch(setSuccessEvent(false))) : null}
+      {sendEmailResponse.success ? toastSuccess('Email inviata correttamente!') && dispatch(setSuccessEmail(false))
+        : onlineMeetingResponse.success ? toastSuccess('Meeting creato correttamente!') && dispatch(setSuccessMeeting(false))
+          : createEventResponse.success ? toastSuccess('Evento creato correttamente!') && dispatch(setSuccessEvent(false)) : null}
 
-      {sendEmailResponse.rejected.message ? toastError((sendEmailResponse.rejected.message + '\n' + authResponse.userPrincipalName), dispatch(setRejectedEmail('')))
-        : onlineMeetingResponse.rejected.message ? toastError((onlineMeetingResponse.rejected.message + '\n' + authResponse.userPrincipalName), dispatch(setRejectedMeeting('')))
-          : createEventResponse.rejected.message ? toastError((createEventResponse.rejected.message + '\n' + authResponse.userPrincipalName), dispatch(setRejectedEvent(''))) : null}
+      {sendEmailResponse.rejected.message ? toastError((sendEmailResponse.rejected.message + '\n' + authResponse.userPrincipalName)) && dispatch(setRejectedEmail(''))
+        : onlineMeetingResponse.rejected.message ? toastError((onlineMeetingResponse.rejected.message + '\n' + authResponse.userPrincipalName)) && dispatch(setRejectedMeeting(''))
+          : createEventResponse.rejected.message ? toastError((createEventResponse.rejected.message + '\n' + authResponse.userPrincipalName)) && dispatch(setRejectedEvent('')) : null}
 
       {/* {props.sendEmailResponse.success ? toastSuccess('Email inviata correttamente!', dispatch(setSuccessEmail(false)))
         : props.onlineMeetingResponse.success ? toastSuccess('Meeting creato correttamente!', dispatch(setSuccessMeeting(false)))
